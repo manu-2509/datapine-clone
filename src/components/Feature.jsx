cd4302a7526508b1d140ab79e57bbe0cbde0d2bf
manu-2509 <maneshs111@gmail.com>
2022-09-07 20:20:53 +0530

    over

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,38 +1,38 @@
 import "./style.css"
 export const Feature=()=>{
     return(
-        <div>
-            <h1>MASTER YOUR ANALYTICS JOURNEY IN ONE SINGLE BI PLATFORM</h1>
-            <div>
-                <div>
-                    <img src="https://www.datapine.com/images/connect-icon.png" alt="" />
-                    <h2>CONNECT</h2>
-                    <p>Fast & easy data connectors</p>
+        <div className="feature-main">
+            <h1 className="feature-heading">MASTER YOUR ANALYTICS JOURNEY IN ONE SINGLE BI PLATFORM</h1>
+            <div className="feature-div">
+                <div className="feature-box">
+                    <img className="feature-img" src="https://www.datapine.com/images/connect-icon.png" alt="" />
+                    <h2 className="feature-head">CONNECT</h2>
+                    <p className="feature-p">Fast & easy data connectors</p>
                 </div>
-                <div>
-                    <img src="https://www.datapine.com/images/connect-icon.png" alt="" />
-                    <h2>CONNECT</h2>
-                    <p>Fast & easy data connectors</p>
+                <div className="feature-box">
+                    <img className="feature-img" src="https://www.datapine.com/images/explore-icon.png" alt="" />
+                    <h2 className="feature-head">EXPLORE</h2>
+                    <p className="feature-p">True self-service analytics features</p>
                 </div>
-                <div>
-                    <img src="https://www.datapine.com/images/connect-icon.png" alt="" />
-                    <h2>CONNECT</h2>
-                    <p>Fast & easy data connectors</p>
+                <div className="feature-box">
+                    <img className="feature-img" src="https://www.datapine.com/images/visualize-icon.png" alt="" />
+                    <h2 className="feature-head">VISUALIZE</h2>
+                    <p className="feature-p">Powerful, interactive BI dashboards</p>
                 </div>
-                <div>
-                    <img src="https://www.datapine.com/images/connect-icon.png" alt="" />
-                    <h2>CONNECT</h2>
-                    <p>Fast & easy data connectors</p>
+                <div className="feature-box">
+                    <img className="feature-img" src="https://www.datapine.com/images/share-icon.png" alt="" />
+                    <h2 className="feature-head">SHARE</h2>
+                    <p className="feature-p">Multiple, effective reporting options</p>
                 </div>
-                <div>
-                    <img src="https://www.datapine.com/images/connect-icon.png" alt="" />
-                    <h2>CONNECT</h2>
-                    <p>Fast & easy data connectors</p>
+                <div className="feature-box">
+                    <img className="feature-img" src="https://www.datapine.com/images/predict-icon.png" alt="" />
+                    <h2 className="feature-head">PREDICT</h2>
+                    <p className="feature-p">Modern predictive analytics tools</p>
                 </div>
-                <div>
-                    <img src="https://www.datapine.com/images/connect-icon.png" alt="" />
-                    <h2>CONNECT</h2>
-                    <p>Fast & easy data connectors</p>
+                <div className="feature-box">
+                    <img className="feature-img" src="https://www.datapine.com/images/monitor-icon.png" alt="" />
+                    <h2 className="feature-head">MONITER</h2>
+                    <p className="feature-p">Intelligent, AI-based data alerts</p>
                 </div>
             </div>
         </div>
---
+++ src/components/Overview.jsx
@@ -0,0 +1,8 @@
+export const Overview=()=>{
+    return(
+        <div>
+            <h1 className="over-head">TAKE A CLOSER LOOK AT ALL OUR POWERFUL BUSINESS INTELLIGENCE FEATURES</h1>
+            <button className="over-btn">SEE FEATURES OVERVIEW</button>
+        </div>
+    )
+}
